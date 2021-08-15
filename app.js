bf7d533150765c00c4332889e1020efe68c46e35
guillote87 <guillermo27@gmail.com>
2021-08-15 12:28:49 -0300

    process.env

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+
+app.listen(process.env.PORT||3000, ()=>{
     console.log('Servidor funcionando');
 });
-
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
